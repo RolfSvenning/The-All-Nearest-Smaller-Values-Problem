38bd2949dbef1b68fe3ff2092c5768c34ff17d6d
Rolf <au591328@post.au.dk>
2023-10-02 23:55:54 +0200

    qwe

---
+++ test.cpp
@@ -0,0 +1,6 @@
+#include "test.h"
+
+test::test(int i) {
+    age = i;
+}
+
---
+++ test.h
@@ -0,0 +1,13 @@
+#ifndef UNTITLED_TEST_H
+#define UNTITLED_TEST_H
+
+//#include "parlay/primitives.h"
+
+class test {
+public:
+    test(int i);
+    int age;
+};
+
+
+#endif //UNTITLED_TEST_H
