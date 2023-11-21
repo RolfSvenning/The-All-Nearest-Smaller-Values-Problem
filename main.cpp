8c5dc9f600e9755533d3913a3e56ed019efcb044
RolfSvenning <rolfsvenning@gmail.com>
2023-11-21 13:35:35 +0100

    _

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include "Glue/data.h"
 
 int main() {
-    for (int i = 0; i < 0; ++i) {
+    for (int i = 0; i < 0; ++i) { //TODO: revert
       std::array<long, n> A = returnRandomArray();
       //    std::array<int, n> A = {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3}; //set constants to 16
       compareOutputOfNaiveSeqArraySeqStack(A, false);
