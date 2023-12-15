ba2908ef882edb97eff3bc11c02141a3e0a692f5
RolfSvenning <rolfsvenning@gmail.com>
2023-12-15 16:40:41 +0100

    _

--- Experiments/experiments.cpp
+++ Experiments/experiments.cpp
@@ -12,15 +12,15 @@ void speedup(int rounds, int repetitions){
     // DATE
     std::chrono::system_clock::time_point today = std::chrono::system_clock::now();
     time_t tt = std::chrono::system_clock::to_time_t ( today );
-    std::cout << "today is: " << ctime(&tt);
 
     // CREATE FILE
-    std::ofstream f;
-    f.open(std::ctime(&tt) + std::string("speedup.txt"));
+    auto fileName = "../Experiments/Results/" + std::to_string(rounds) + " " + std::to_string(repetitions) + " " + std::ctime(&tt) + std::string(".txt");
+    std::ofstream f (fileName);
+    f.open(fileName); // std::ctime(&tt) + std::string("speedup.txt")
     f << "Writing this to a file.\n";
     f.close();
 
-    std::cout << "Created file maybe?" << std::endl;
+//    std::cout << "Created file maybe?" << std::endl;
 
 }
 
