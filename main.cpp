2fce7c8ad8316e9fef05978892977b4f1604cf1c
Rolf <au591328@post.au.dk>
2024-01-04 00:09:23 +0100

    ran experiments on my desktop (windows)

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ using namespace parlay;
 
 int main() {
 //    long n = 4; // 1234567
-//    const long blockSize = 4; //TODO: only works for even block_size
+//    const long blockSize = 4;
 //    sequence<long> A = returnSortedArray(n);
 //    testArrayShunZhaoAndMineAndBerkman(A, blockSize);
 //    sequence<long> A = {1,1,7,8,5,4,2};
@@ -26,29 +26,28 @@ int main() {
 //    printArraysVI(list<sequence<VI>>{L, R});
 
 
-    long n = 4194304; // 2^24 = 16777216 2^22 = 4194304 2^20 = 1048576 2^18 = 262144 2^16 = 65536
+//    long n = 16777216; // 2^24 = 16777216 2^22 = 4194304 2^20 = 1048576 2^18 = 262144 2^16 = 65536
     int rounds = 3;
-    bool usingHeuristic = true;
-    string algorithmType = "SHUN_ZHAO"; // SEQUENTIAL, SHUN_ZHAO, SHUN_ZHAO_NO_HEURISTIC, BERKMAN_VISHKIN
+//    string algorithmType = "SHUN_ZHAO"; // SEQUENTIAL, SHUN_ZHAO, SHUN_ZHAO_NO_HEURISTIC, BERKMAN_VISHKIN
 //    experiment("speedup", n, algorithmType, "MERGE", rounds);
 //    experiment("speedup", n, algorithmType, "SORTED", rounds);
 //    experiment("speedup", n, algorithmType, "RANDOM", rounds);
 //    experimentAllInputs("speedup", n, algorithmType, rounds);
 
     // SPEEDUP
-//    experimentAllInputsAllAlgorithms("speedup", n, rounds);
+    experimentAllInputsAllAlgorithms("speedup", 16777216, 8192, rounds);
 
     // RUNNING TIME
-//    experimentRunningTime(20, 4096, 3);
+    experimentRunningTime(24, 8192, rounds);
 
     // BLOCK SIZE 64 * 64 = 4096
-//    experimentBlocksize(1048576, 3);
+    experimentBlocksize(1048576, rounds);
 
 
 //    totalTimeExperiment(1, 1);
 //    int putEnvSuccess = _putenv("PARLAY_NUM_THREADS=12");
 //    assert(putEnvSuccess == 0);
-    return testAllCorrectness();
+//    return testAllCorrectness();
 //    cout << "running" << endl;
 //    sequence<long> A = {1,2,3};
 //    cout << A[1] << endl;
