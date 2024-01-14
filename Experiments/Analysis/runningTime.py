063d9d18cfadf55ef531e927aa74854a1953f796
RolfSvenning <rolfsvenning@gmail.com>
2024-01-14 10:00:07 -0800

    _

--- Experiments/Analysis/runningTime.py
+++ Experiments/Analysis/runningTime.py
@@ -1,23 +1,25 @@
 from parseExperiments import parseFile
 from matplotlib import pyplot as plt
 import numpy as np
+from math import log2
 
 AtoC = {"SEQUENTIAL": 'blue', "SHUN_ZHAO": 'green', "BERKMAN_VISHKIN": 'red'} 
 
 def plotRunningTimeAllInputTypes(Es, P, logscale):
     for inputType in ["MERGE", "SHUFFLED_MERGE", "RANDOM", "SORTED"]:
-        plotRunningTime(Es, inputType, P, "Running times (all inputs, all algorithms, blocksize = 8192, P = " + str(P), logscale)
+        plotRunningTime(Es, inputType, P, "Running times (all inputs, all algorithms, blocksize = 256 * log2(n), P = " + str(P), logscale)
 
 def plotRunningTime(NTs_, inputType, p, title, logScale):
     # NTs = [(e.n, e.times, e.algorithmType, e.numberOfCores) for e in NTs_ if (e.inputType == inputType and e.numberOfCores == p)]
-    E = [e for e in NTs_ if (e.inputType == inputType and e.numberOfCores == p and e.n >= 64)]
+    E = [e for e in NTs_ if (e.inputType == inputType and e.numberOfCores == p and e.n >= 1000)] # TODO: excluding small n
     Cmap = {a:AtoC[a] for a in set([e.algorithmType for e in E]) } 
 
     _, ax = plt.subplots()
     # N = np.array([e.n for e in NTs])
     # T = np.array([e.times for e in NTs])
     N = np.array([e.n for e in E])
-    Tnorm = np.array([e.averageTime / e.n for e in E])
+    Tnorm = np.array([e.averageTime / (e.n)for e in E]) # TODO: OR DIVIDE BY nlogn !
+    T = np.array([e.averageTime for e in E])
     Terr = np.hstack([[max(0, (e.averageTime - e.minTimes) / e.n), max(0, (e.maxTime - e.averageTime) / e.n)] for e in E])
     C = np.array([Cmap[e.algorithmType] for e in E])
 
@@ -25,9 +27,10 @@ def plotRunningTime(NTs_, inputType, p, title, logScale):
         ix = np.where(C == color)
         N2 = N[ix]
         Tnorm2 = Tnorm[ix]
+        T2 = T[ix]
         Terr2 = Terr[ix]
-        # ax.scatter(N[ix], T[ix] / N[ix], c=color, label=algorithmType + " " + inputType, s=10)
-        ax.errorbar(N2, Tnorm2, Terr2, fmt='o', c=color, label=algorithmType + " " + inputType, ms=2)
+        ax.scatter(N2, Tnorm2, c=color, label=algorithmType + " " + inputType, s=10)
+        # ax.errorbar(N2, Tnorm2, Terr2, fmt='o', c=color, label=algorithmType + " " + inputType, ms=2)
         
     if logScale: ax.set_xscale('log')
     ax.set_xlabel('n')
@@ -37,9 +40,9 @@ def plotRunningTime(NTs_, inputType, p, title, logScale):
     plt.show()
 
 Es = parseFile("runningTime.txt")
-plotRunningTimeAllInputTypes(Es, 1, True)
+# plotRunningTimeAllInputTypes(Es, 1, True)
 # plotRunningTimeAllInputTypes(Es, 48, True)
 
 # Without sequential for P = 48
-# Es = [e for e in parseFile("runningTime.txt") if e.algorithmType != "SEQUENTIAL"]
-# plotRunningTimeAllInputTypes(Es, 48, False)
+Es = [e for e in parseFile("runningTime.txt") if e.algorithmType != "SEQUENTIAL"]
+plotRunningTimeAllInputTypes(Es, 48, True)
