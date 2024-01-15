f638b84e735e0251a90a88db5c0c2bf80c3d7b15
RolfSvenning <rolfsvenning@gmail.com>
2024-01-14 21:40:12 -0800

    plotting for blocksize

--- Experiments/Analysis/parseExperiments.py
+++ Experiments/Analysis/parseExperiments.py
@@ -1,7 +1,7 @@
 import re
 
 class E:
-    def __init__(self, algorithmType, inputType, numberOfCores, n, blockSize, usingHeuristic, times):
+    def __init__(self, algorithmType, inputType, numberOfCores, n, blockSize, usingHeuristic, times, T_tree, T_local, T_merging):
         self.algorithmType  = algorithmType
         self.inputType      = inputType
         self.numberOfCores  = int(numberOfCores)
@@ -12,6 +12,9 @@ class E:
         self.averageTime    = sum(times) / len(times)
         self.maxTime        = max(times)
         self.minTimes       = min(times)
+        self.T_tree         = -1 if len(T_tree)    == 0 else sum(T_tree)    / len(T_tree)
+        self.T_local        = -1 if len(T_local)   == 0 else sum(T_local)   / len(T_local)
+        self.T_merging      = -1 if len(T_merging) == 0 else sum(T_merging) / len(T_merging)
 
         assert 1 <= self.numberOfCores <= 48
         assert self.times
@@ -40,8 +43,16 @@ def parseExperiment(e):
     algorithmType, inputType, _, numberOfCores = re.findall("=(\w+)", e)
     n, blockSize, usingHeuristic = re.findall("\d+", e.split("\n")[3])
     times = [float(t) for t in re.findall("Total time: ([\d|.]+)", e)]
-    return E(algorithmType, inputType, numberOfCores, n, blockSize, usingHeuristic, times)
+    T_tree = [float(t) for t in re.findall("Binary tree: ([\d|.]+)", e)]
+    T_local = [float(t) for t in re.findall("Local matches: ([\d|.]+)", e)]
+    T_merging = [float(t) for t in re.findall("Merging: ([\d|.]+)", e)]
+    T_treeTraversal = [float(t) for t in re.findall("Tree traversal: ([\d|.]+)", e)]
+    return E(algorithmType, inputType, numberOfCores, n, blockSize, usingHeuristic, times, T_tree, T_local, T_merging + T_treeTraversal)
 
+# Binary tree: 0.001024
+# Local matches: 0.015288
+# Merging: 0.011013
+# Total time: 0.027328
 
 def parseFile(fileName):
     Es = [parseExperiment(e) for e in open("Experiments/Results/" + fileName).read().split("Date")[1:]]# TODO[1:]
---
+++ Experiments/Analysis/speedup.py
@@ -0,0 +1,51 @@
+from parseExperiments import parseFile
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+AtoC = {"SEQUENTIAL": 'blue', "SHUN_ZHAO": 'orange', "BERKMAN_VISHKIN": 'black'} 
+AtoM = {"SEQUENTIAL": 'x', "SHUN_ZHAO": 'o', "BERKMAN_VISHKIN": '^'} 
+
+
+def plotSpeedup(E, title, logScale, showSpeedup=False):
+    _, ax = plt.subplots()
+    P = np.array([e.numberOfCores for e in E])
+
+    assert E[0].numberOfCores == 1
+    T1 = E[0].averageTime
+
+    if showSpeedup: Tnorm = np.array([T1 / e.averageTime for e in E])
+    else: Tnorm = np.array([e.averageTime / e.n for e in E])
+
+    C = np.array([AtoC[e.algorithmType] for e in E])
+
+    for algorithmType, color in AtoC.items():
+        ix = np.where(C == color)
+        P2 = P[ix]
+        Tnorm2 = Tnorm[ix]
+
+        if showSpeedup: Pall, Tall = zip(*[(e.numberOfCores, T1 / t) for e in np.array(E)[ix] for t in e.times])
+        else: Pall, Tall = zip(*[(e.numberOfCores, t / e.n) for e in np.array(E)[ix] for t in e.times])
+        
+        ax.scatter(Pall, Tall, c=color, s=10, marker=AtoM[algorithmType])
+                
+        P2, Tnorm2 = zip(*[(n, t) for n, t in sorted(list(zip(P2, Tnorm2)), key=lambda x: x[0])])
+        ax.plot(P2, Tnorm2, c=color, label=algorithmType + " " + algorithmType)
+
+        
+    if logScale: ax.set_xscale('log')
+    ax.set_xlabel('P')
+    ax.set_ylabel('seconds')
+    ax.set_title(title)
+    ax.legend()
+    plt.show()
+
+
+Es = parseFile("speedup.txt")
+
+# P = 1
+plotSpeedup(Es, "Number of Processors vs. Average Running Time (blocksize = 256 * log2(n))", logScale=False)
+plotSpeedup(Es, "Number of Processors vs. T1 / Average Running Time (blocksize = 256 * log2(n))", logScale=False, showSpeedup=True)
+
+# P = 48
+# plotBlocksize(Es, 12, "Block Size vs. Average Running Time (n = xxx, P = 48)", logScale=True)
