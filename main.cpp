2af554debf15a7c93931b1bc795b2ae62e0f0d41
RolfSvenning <rolfsvenning@gmail.com>
2024-01-24 22:50:25 -0800

    added readme

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
-#include "Glue/_aux.h"
+
 #include "iostream"
+
 #include "Experiments/experiments.h"
+#include "Glue/_aux.h"
 #include "Glue/data.h"
 #include "Testing/TestANSV.h"
+
 
 using namespace std;
 using namespace parlay;
@@ -12,111 +15,31 @@ int main(int argc, char* argv[]) {
     std::cerr << "Usage: " << argv[0] << " <experiment_type> <n> <block_size> <repetitions>\n";
     return 1;
   }
-//  auto [size, logSize] = largestN("BERKMAN_VISHKIN", "MERGE", 8192);
-//        cout << "largest n:" << size << endl;
-    cout << sizeof(long) << endl;
 
-// "SEQUENTIAL" "SEQUENTIAL_STACK" "SHUN_ZHAO" "SHUN_ZHAO_NO_HEURISTIC" "BERKMAN_VISHKIN"
+
+  // "SEQUENTIAL" "SEQUENTIAL_STACK" "SHUN_ZHAO" "SHUN_ZHAO_NO_HEURISTIC" "BERKMAN_VISHKIN"
   string experimentType = argv[1];
   long n = atol(argv[2]);
   long blockSize = atol(argv[3]);
   int repetitions = atoi(argv[4]);
-//  for (int i = 0; i < repetitions; i++) {
-//    cout << "repetition " << i + 1 << endl;
-//      sequence <long> A1 = returnRandomArray(n);
-////  testArrayShunZhaoAndMineAndBerkman(A, blockSize);
-//      auto [L1, R1, tStack] = ANSV_generic("BERKMAN_VISHKIN", A1, blockSize);
-//      sequence <long> A2 = returnShuffledMergeArray(n);
-//
-//      auto [L2, R2, tArray]  = ANSV_generic("BERKMAN_VISHKIN", A2, blockSize);
-////      printArray(L1);
-////      printArray(L2);
-////      assert (L1 == L2);
-////      assert (R1 == R2);
-//      cout << tStack << endl;
-//      cout << tArray << endl;
-//  }
 
-//
-//        // EXPERIMENTS
-//        if (experimentType == "speedup") {
-//            // SPEEDUP // Fix N //
-//            experimentSpeedup(n, -1, repetitions);
-//        } else if (experimentType == "running_time") {
-//            // RUNNING TIME // Fix P=1, maxNumberOfProcessors. //
-//            experimentRunningTime(n, blockSize, repetitions);
-//        } else if (experimentType == "block_size") {
-//            // BLOCK SIZE // Fix N and P. //
-//            experimentBlocksize(n, blockSize, repetitions);
-//        } else {
-//          cerr << "Invalid experiment type\n";
-//          return -1;
-//        }
-    return testAllCorrectness();
-//return 1;
+
+  //
+  //        // EXPERIMENTS
+  //        if (experimentType == "speedup") {
+  //            // SPEEDUP // Fix N //
+  //            experimentSpeedup(n, -1, repetitions);
+  //        } else if (experimentType == "running_time") {
+  //            // RUNNING TIME // Fix P=1, maxNumberOfProcessors. //
+  //            experimentRunningTime(n, blockSize, repetitions);
+  //        } else if (experimentType == "block_size") {
+  //            // BLOCK SIZE // Fix N and P. //
+  //            experimentBlocksize(n, blockSize, repetitions);
+  //        } else {
+  //          cerr << "Invalid experiment type\n";
+  //          return -1;
+  //        }
+  return testAllCorrectness();
+  //return 1;
 }
-// PARLAY_NUM_THREADS=1
 
-
-
-////    long largestN(const string& algorithmType, const string& inputType, string (*ANSV)(long*, long, long*, long*, long, bool), long BLOCK_SIZE){
-//assert(getenv("PARLAY_NUM_THREADS") != nullptr);
-////    auto [n, logn] = largestN("BERKMAN_VISHKIN", "MERGE", 8192);
-//cout << "largest n:" << n << endl;
-//cout << "number of threads:" << getenv("PARLAY_NUM_THREADS") << endl;
-//cout << "number of threads:" << strtol(getenv("PARLAY_NUM_THREADS"), nullptr, 10) << endl;
-//setenv("PARLAY_NUM_THREADS", "9", 1);
-//cout << "number of threads:" << getenv("PARLAY_NUM_THREADS") << endl;
-//setenv("PARLAY_NUM_THREADS", "12", 1);
-//cout << "number of threads:" << getenv("PARLAY_NUM_THREADS") << endl;
-//
-////    n = 65536; //2^16 = 65536 2^18 = 262144 2^20 = 1048576 2^22 = 4194304 2^24 = 16777216
-////    const char *si;
-////    // SPEEDUP
-//for (long i = 1; i <= strtol(getenv("PARLAY_NUM_THREADS"), nullptr, 10); i++) {
-//  string stringIndex = to_string(i);
-//  char * si = new char [stringIndex.length()+1];
-//  std::strcpy (si, stringIndex.c_str());
-//  int putEnvSuccess = setenv("PARLAY_NUM_THREADS", si, 1);
-//  cout << "number of threads:" << getenv("PARLAY_NUM_THREADS") << endl;
-//  assert(putEnvSuccess == 0);
-//  experimentAllInputsAllAlgorithms("speedup", n, 8192, repetitions);
-//}
-//
-////     RUNNING TIME P=1, maxNumberOfProcessors
-//for(long i: {1l, strtol(getenv("PARLAY_NUM_THREADS"), nullptr, 10)}){
-//  cout << "number of threads:" << getenv("PARLAY_NUM_THREADS") << endl;
-//  int putEnvSuccess = setenv("PARLAY_NUM_THREADS", to_string(i).c_str(), 1);
-//  assert(putEnvSuccess == 0);
-//  experimentRunningTime(16, 8192, repetitions);
-//}
-
-//    long n = 4; // 1234567
-//    const long blockSize = 4;
-//    sequence<long> A = returnSortedArray(n);
-//    testArrayShunZhaoAndMineAndBerkman(A, blockSize);
-//    sequence<long> A = {1,1,7,8,5,4,2};
-//    auto [L, R] = ANSV_Berkman(A, blockSize);
-//    printArray(A);
-//    printArraysVI(list<sequence<VI>>{L, R});
-
-
-//    long n = 16777216; // 2^24 = 16777216 2^22 = 4194304 2^20 = 1048576 2^18 = 262144 2^16 = 65536
-
-
-//    totalTimeExperiment(1, 1);
-//    int putEnvSuccess = _putenv("PARLAY_NUM_THREADS=12");
-//    assert(putEnvSuccess == 0);
-//    return testAllCorrectness();
-//    cout << "running" << endl;
-//    sequence<long> A = {1,2,3};
-//    std::cout << 123 << std::endl;
-//    std::cout << ftest(9) << std::endl;
-
-
-//    int repetitions = 3;
-//    string algorithmType = "SHUN_ZHAO"; // SEQUENTIAL, SHUN_ZHAO, SHUN_ZHAO_NO_HEURISTIC, BERKMAN_VISHKIN
-//    experiment("speedup", n, algorithmType, "MERGE", rounds);
-//    experiment("speedup", n, algorithmType, "SORTED", rounds);
-//    experiment("speedup", n, algorithmType, "RANDOM", rounds);
-//    experimentAllInputs("speedup", n, algorithmType, rounds);
---
+++ readme.md
@@ -0,0 +1,67 @@
+ANSV2: All Nearest Smaller Values Problem
+==========================================
+
+Overview:
+---------
+The ANSV2 project is focused on solving the All Nearest Smaller Values (ANSV) problem, a fundamental problem in computational theory with a wide array of applications. In this problem, given an array A of n totally ordered distinct elements, the goal is to compute for each element in the array the indices of the nearest smaller values to its left and right.
+
+This repository includes various algorithmic implementations to solve the ANSV problem efficiently, and highlights a range of applications.
+
+Features:
+---------
+- Implementations of the ANSV problem using various algorithmic approaches:
+- Sequential array-based solution with O(n) complexity.
+- The Berkman, Schieber, and Vishkin Optimal solution.
+- The original Shun and Zhao approach.
+
+Dependencies:
+-------------
+- CMake (minimum required version 3.16).
+- C++20 Standard for modern C++ features.
+- ParlayLib (header-only library) for efficient parallel computations.
+- Pthread library for multithreading support.
+
+Building the Project:
+---------------------
+1. Ensure CMake (version 3.16 or higher) and a C++20 compliant compiler are installed.
+2. Clone this repository to your system.
+3. Navigate to the project directory and run the following commands:
+   cmake .
+   make
+4. This will compile the ANSV2 executable.
+
+Usage:
+------
+- Execute the ANSV2 executable to run the algorithms.
+- The main.cpp file serves as the entry point and can be modified to test different algorithms or inputs.
+
+Applications of ANSV:
+---------------------
+The ANSV problem has numerous applications in various domains, including but not limited to:
+1. Finding the min/max among n elements.
+2. Merging sorted lists.
+3. Constructing Cartesian trees.
+4. Monotone polygon triangulation.
+5. Range minimum queries.
+6. Parenthesis matching.
+7. Binary tree reconstruction.
+(For more details, see berkman1993)
+
+References:
+-----------
+- [berkman1993] **O. Berkman, B. Schieber, and U. Vishkin. 1993.** "Optimal Doubly Logarithmic Parallel Algorithms Based On Finding All Nearest Smaller Values." *Journal of Algorithms* 14, no. 3 (1993): 344-370. [DOI: 10.1006/jagm.1993.1018](https://doi.org/10.1006/jagm.1993.1018)
+
+- [zhao2013] Julian Shun and Fuyao Zhao. 2013.** "Practical Parallel Lempel-Ziv Factorization." In *2013 Data Compression Conference*. IEEE, 123–132. [DOI: 10.1109/DCC.2013.20](https://doi.org/10.1109/DCC.2013.20)
+
+License:
+--------
+
+This code is part of the paper The All Nearest Smaller Values Problem Revisited in Practice, Parallel and External Memory.
+
+Copyright (c) [2024] [Anonymous authors]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
