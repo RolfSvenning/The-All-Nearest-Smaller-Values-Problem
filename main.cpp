991dbfe1857950c6e4a71d495b1a10326bb17b99
RolfSvenning <rolfsvenning@gmail.com>
2024-03-21 21:56:24 -0700

    set up main for single input for branch misses

--- main.cpp
+++ main.cpp
@@ -18,11 +18,30 @@ int main(int argc, char* argv[]) {
 
 
   // "SEQUENTIAL" "SEQUENTIAL_STACK" "SHUN_ZHAO" "SHUN_ZHAO_NO_HEURISTIC" "BERKMAN_VISHKIN"
-  string experimentType = argv[1];
+//  string experimentType = argv[1];
+  string inputType = argv[1]; // shuffledMerge or random
   long n = atol(argv[2]);
   long blockSize = atol(argv[3]);
-  int repetitions = atoi(argv[4]);
+//  int repetitions = atoi(argv[4]);
+  string algorithmType = argv[4]; // SHUN_ZHAO or BERKMAN_VISHKIN
+  // print input arguments
+  cout << "Input arguments: " << inputType << " " << n << " " << blockSize << " " << algorithmType << endl;
 
+
+      // string ANSV_generic_experiments(const string& algorithmType, parlay::sequence<long> &A_, long blockSize) {
+  // input
+  parlay::sequence<long> A;
+  if (inputType == "shuffledMerge") {
+    A = returnShuffledMergeArray(n);
+  } else if (inputType == "random") {
+    A = returnRandomArray(n);
+  } else {
+    cerr << "Invalid input type\n";
+    return -1;
+  }
+  string time = ANSV_generic_experiments(algorithmType, A, blockSize);
+  cout << time << endl;
+//  sequence<long> returnRandomArray(long n);
 
   //
   //        // EXPERIMENTS
@@ -39,7 +58,7 @@ int main(int argc, char* argv[]) {
   //          cerr << "Invalid experiment type\n";
   //          return -1;
   //        }
-  return testAllCorrectness();
+//  return testAllCorrectness();
   //return 1;
 }
 
